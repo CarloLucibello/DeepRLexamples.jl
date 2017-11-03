570a40ddd01a52b8395716ad1a8b36e64af430aa
CarloLucibello <carlo.lucibello@gmail.com>
2017-11-03 02:10:20 +0100

    speedup

---
+++ examples/python/reinforce_pytorch.py
@@ -0,0 +1,95 @@
+import argparse
+import gym
+import numpy as np
+from itertools import count
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torch.autograd as autograd
+from torch.autograd import Variable
+
+
+parser = argparse.ArgumentParser(description='PyTorch REINFORCE example')
+parser.add_argument('--gamma', type=float, default=0.99, metavar='G',
+                    help='discount factor (default: 0.99)')
+parser.add_argument('--seed', type=int, default=543, metavar='N',
+                    help='random seed (default: 543)')
+parser.add_argument('--render', action='store_true',
+                    help='render the environment')
+parser.add_argument('--log_interval', type=int, default=10, metavar='N',
+                    help='interval between training status logs (default: 10)')
+args = parser.parse_args()
+
+
+env = gym.make('CartPole-v1')
+env.seed(args.seed)
+torch.manual_seed(args.seed)
+
+
+class Policy(nn.Module):
+    def __init__(self):
+        super(Policy, self).__init__()
+        self.affine1 = nn.Linear(4, 128)
+        self.affine2 = nn.Linear(128, 2)
+
+        self.saved_actions = []
+        self.rewards = []
+
+    def forward(self, x):
+        x = F.relu(self.affine1(x))
+        action_scores = self.affine2(x)
+        return F.softmax(action_scores)
+
+
+policy = Policy()
+optimizer = optim.Adam(policy.parameters(), lr=1e-3)
+
+
+def select_action(state):
+    state = torch.from_numpy(state).float().unsqueeze(0)
+    probs = policy(Variable(state))
+    action = probs.multinomial()
+    policy.saved_actions.append(action)
+    return action.data
+
+
+def finish_episode():
+    R = 0
+    rewards = []
+    for r in policy.rewards[::-1]:
+        R = r + args.gamma * R
+        rewards.insert(0, R)
+    rewards = torch.Tensor(rewards)
+    rewards = (rewards - rewards.mean()) / (rewards.std() + np.finfo(np.float32).eps)
+    for action, r in zip(policy.saved_actions, rewards):
+        action.reinforce(r)
+    optimizer.zero_grad()
+    autograd.backward(policy.saved_actions, [None for _ in policy.saved_actions])
+    optimizer.step()
+    del policy.rewards[:]
+    del policy.saved_actions[:]
+
+
+running_reward = 10
+for i_episode in count(1):
+    state = env.reset()
+    for t in range(10000): # Don't infinite loop while learning
+        action = select_action(state)
+        state, reward, done, _ = env.step(action[0,0])
+        if args.render:
+            env.render()
+        policy.rewards.append(reward)
+        if done:
+            break
+
+    running_reward = running_reward * 0.99 + t * 0.01
+    finish_episode()
+    if i_episode % args.log_interval == 0:
+        print('Episode {}\tLast length: {:5d}\tAverage length: {:.2f}'.format(
+            i_episode, t, running_reward))
+    if running_reward > env.spec.reward_threshold:
+        print("Solved! Running reward is now {} and "
+              "the last episode runs to {} time steps!".format(running_reward, t))
+        break
--- examples/reinforce_knet.jl
+++ examples/reinforce_knet.jl
@@ -4,6 +4,8 @@ using DeepRL
 using ArgParse
 import AutoGrad: getval
 
+const F = Float64
+
 function main(args)
     s = ArgParseSettings()
     s.description = ""
@@ -11,29 +13,29 @@ function main(args)
     @add_arg_table s begin
         ("--hidden"; default=[100]; nargs='*'; arg_type=Int64)
         ("--atype"; default=(gpu()>=0 ? "KnetArray{Float32}":"Array{Float64}"))
-        ("--optim"; default="Adam(;lr=1e-4)")
-        ("--discount"; default=0.95; arg_type=Float64)
-        ("--episodes"; default=1000; arg_type=Int64)
+        ("--optim"; default="Adam(;lr=1e-2)")
+        ("--discount"; default=0.99; arg_type=Float64)
+        ("--episodes"; default=500; arg_type=Int64)
+        ("--rendered"; default=true; arg_type=Bool)
         ("--seed"; default=-1; arg_type=Int64)
-        ("--period"; default=100; arg_type=Int64)
+        ("--period"; default=50; arg_type=Int64)
     end
-    rendered = true
 
     isa(args, String) && (args = split(args))
     o = parse_args(args, s; as_symbols=true); println(o)
     o[:atype] = eval(parse(o[:atype]))
-    o[:seed] > 0 && srand(o[:seed])
-
-    threshold =  500
+    
     env = GymEnv("CartPole-v1")
-    w = initweights(o[:atype], o[:hidden])
+    o[:seed] > 0 && (srand(o[:seed]); srand(env, o[:seed]))
+    xsize, ysize = 4, 2 
+    w = initweights(o[:atype], o[:hidden], xsize, ysize)
     opts = map(wi->eval(parse(o[:optim])), w)
 
     avgreward = 0
     for k = 1:o[:episodes]
         state = reset!(env)
         episode_rewards = 0
-        history = EntryHist[]
+        history = History()
         for t=1:1000
             s = convert(o[:atype], state)
             pred = predict(w,s)
@@ -41,39 +43,38 @@ function main(args)
             action = sample_action(probs)
 
             next_state, reward, done, _ = step!(env, actions(env)[action])
-            push!(history, EntryHist(state, action, reward))
+            append!(history.states, state)
+            push!(history.actions, action)
+            push!(history.rewards, reward)
             state = next_state
             episode_rewards += reward
 
-            k % o[:period] == 0 && rendered && (render(env); sleep(0.05))
+            k % o[:period] == 0 && o[:rendered] && render(env)
             done && break
         end
 
         avgreward = 0.01 * episode_rewards + avgreward * 0.99
 
         k % o[:period] == 0 && println("(episode:$k, avgreward:$avgreward)")
-        k % o[:period] == 0 && rendered && render(env, close=true)
+        k % o[:period] == 0 && o[:rendered] && render(env, close=true)
 
-        values = get_values(history, o[:discount]; threshold=threshold)
-        for t=1:length(history)
-            state = history[t].state
-            action = history[t].action
-            value = values[t]
-            train!(w, state, action, value, opts)
-        end
+        states = reshape(history.states, xsize, length(history.states)÷xsize)
+        values = get_values(history.rewards, o[:discount])
+        train!(w, states, history.actions, values, opts)
     end
 end
 
-struct EntryHist
-    state
-    action
-    reward
+ mutable struct History
+    states::Vector{F}
+    actions::Vector{Int}
+    rewards::Vector{F}
 end
+History() = History(zeros(F,0),zeros(Int, 0),zeros(F,0))
 
-function initweights(atype, hidden, xsize=4, ysize=2, winit=0.1)
+function initweights(atype, hidden, xsize=4, ysize=2)
     w = []
     x = xsize
-    for y in [hidden..., 2]
+    for y in [hidden..., ysize]
         push!(w, xavier(y, x))
         push!(w, zeros(y, 1))
         x = y
@@ -82,6 +83,7 @@ function initweights(atype, hidden, xsize=4, ysize=2, winit=0.1)
 end
 
 function predict(w,x)
+    # @show size(w) size(x)
     for i = 1:2:length(w)-2
         x = relu.(w[i] * x .+ w[i+1])
     end
@@ -103,31 +105,29 @@ function sample_action(probs)
     return actions[1]
 end
 
-function get_values(history, γ; threshold=500)
-    T = typeof(history[1].reward)
-    values = Array{T}(length(history)+1)
-    if length(history) < threshold
-        values[end] = length(history) - threshold
-    else
-        values[end] = 0
+function get_values(rewards, γ)
+    values = similar(rewards)
+    values[end] = rewards[end]
+    for k = length(rewards)-1:-1:1
+        values[k] = γ * values[k+1] + rewards[k]
     end
-    for k = length(history):-1:1
-        values[k] = γ * values[k+1] + history[k].reward
-    end
-    return values[1:end-1]
+
+    return (values .- mean(values)) ./ (std(values) + F(1e-8))
 end
 
 function loss(w, x, actions, values)
+    # @show size(x)
     ypred = predict(w,x)
     nrows, ncols = size(ypred)
     index = actions + nrows*(0:(length(actions)-1))
+    # @show size(ypred) actions values index
     lp = logp(ypred, 1)[index]
     return -sum(lp .* values) / size(x, 2)
 end
 
 function train!(w, s, a, v, opt)
-    g = grad(loss)(w, s, a, v)
-    update!(w, g, opt)
+    dw = grad(loss)(w, s, a, v)
+    update!(w, dw, opt)
 end
 
 !isinteractive() && main(ARGS)
